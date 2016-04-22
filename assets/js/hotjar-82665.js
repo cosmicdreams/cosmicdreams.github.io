c1470863cdd6d56e312c4348a3124a87929b5d16
Chris Weber <christopher.m.weber@gmail.com>
2016-04-21 21:36:18 -0400

    Complete version 1 of the website

---
+++ assets/js/hotjar-82665.js
@@ -0,0 +1,9 @@
+window.hjSiteSettings = {"testers_widgets":[],"recording_capture_keystrokes":true,"site_id":82665,"deferred_page_contents":[],"record_targeting_rules":[],"surveys":[],"heatmaps":[{"targeting":[{"negate":false,"pattern":"http:\/\/www.mikelseverson.com\/","match_operation":"simple","component":"url"}],"created_epoch_time":1453714079,"id":331614}],"polls":[],"forms":[],"record":true,"r":1.0};
+
+window.hjBootstrap = function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+};
+
+hjBootstrap('https://script.hotjar.com/modules-41583c52937b4bb5735122fea5d73169.js');
